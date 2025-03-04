4d988a4758e33036fb9182e3f72ac2fac8fce694
gprok <gprok60@gmail.com>
2025-03-04 19:17:52 +0200

    Singly Linked List revision

--- 02-LinkedLists/single_linked_list.py
+++ 02-LinkedLists/single_linked_list.py
@@ -1,29 +1,55 @@
 
-class LisnkedListNode:
+class ListNode:
     def __init__(self, value):
         self.value = value
-        self.next = None
+        self.next = None 
 
-class LisnkedList:
+
+class SinglyLinkedList:
     def __init__(self):
         self.head = None 
 
     def add(self, value):
-        new_node = LisnkedListNode(value)
-        new_node.next = self.head 
+        new_node = ListNode(value)
+        new_node.next = self.head
         self.head = new_node
 
     def display(self):
-        iter = self.head 
-        while iter is not None:
-            print(iter.value)
-            iter = iter.next
-    
-list = LisnkedList()
-list.add(30)
-list.add(36)
-list.add(14)
-list.display()
+        iterator = self.head
+        while iterator is not None:
+            print(iterator.value)
+            iterator = iterator.next
+
+    def remove(self, value): 
+        iterator = self.head
+        previous_iterator = None 
+        while iterator is not None:
+            if iterator.value == value:
+                if previous_iterator is None:    # it's the first node
+                    self.head = iterator.next
+                else:
+                    previous_iterator.next = iterator.next 
+                return True
+            previous_iterator = iterator
+            iterator = iterator.next
+        return False
 
 
 
+
+data = SinglyLinkedList() 
+data.add(35)
+data.add(56)
+data.add(10)
+data.add(21)
+data.add(67)
+data.display()
+print("REMOVE 10")
+data.remove(10)
+data.display()
+print("REMOVE 35 - LAST ONE")
+data.remove(35)
+data.display()
+print("REMOVE 67 - FIRST ONE")
+data.remove(67)
+data.display()
